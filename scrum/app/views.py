130713b375d7f359d5f354dd460a3c2d3b9ce114
renatomgg <renato.mayeregger@gmail.com>
2023-03-25 20:57:44 -0300

    Login-Logout

--- scrum/app/views.py
+++ scrum/app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from django.conf import settings
 
 # Create your views here.
+
+
+def login_usuario(request):
+    if request.method=='POST':
+        usuario=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(username=usuario,password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('inicio')
+        else:
+            messages.error (request, "Usuario o contraseña incorrecta")
+            return redirect('login')
+    return render(request,"Login.html",{})
+
+def inicio (request):
+    return render(request,"inicio.html",{})
+
+def cerrar (request):
+    logout(request)
+    return redirect('login')
