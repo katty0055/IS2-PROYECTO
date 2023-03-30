f1e8d040950b923966d7f7d7607964cdaee78781
Katteryne Gaona <katteryne0055@fpuna.edu.py>
2023-03-30 02:52:02 -0400

    pantalla de inicio

--- scrum/scrum/urls.py
+++ scrum/scrum/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from app import views
 
 urlpatterns = [
@@ -25,5 +27,6 @@ urlpatterns = [
     
 ]
 
-
-
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
