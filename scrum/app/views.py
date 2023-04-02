96cbac2fff4e5b631d83fce2ef0f7d0e6a55491f
Johan Gonzalez <johangonzalez790@fpuna.edu.py>
2023-04-01 20:43:36 -0400

    CONTINUACION CREAR USUARIOS

--- scrum/app/views.py
+++ scrum/app/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.conf import settings
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth.models import User
+from django.utils import timezone
 
     
 
@@ -30,7 +31,11 @@ def cerrar (request):
     logout(request)
     return redirect('login')
 
+def crear_proyecto (request):
+    return render(request,"crear_proyecto.html",{})
+
 def crear_usuario(request):
+    print("hola")
     if request.method=='POST':
         username=request.POST['usuario']
         first_name=request.POST['nombre']
@@ -41,18 +46,41 @@ def crear_usuario(request):
         is_staff=False
         is_superuser=False
         reingresar_contrasenha=request.POST['reingresar_contrasenha']
-        if check_password(reingresar_contrasenha, password):
-            print("Coinciden")
-        else: 
-            messages.error (request, "La contraseña no coincide")
-        obj=User()
-        obj.username=username
-        obj.first_name=first_name
-        obj.last_name=last_name
-        obj.email=email
-        obj.password=password
-        obj.is_active=is_active
-        obj.is_staff=is_staff
-        obj.is_superuser=is_superuser
-        print(obj)
+        date_joined=timezone.now()
+        print(username)
+        obj=User.objects.create(username=username, first_name=first_name, last_name=last_name,
+                                email=email, password=password, is_active=is_active, is_staff=is_staff,
+                                is_superuser=is_superuser, date_joined=date_joined)
+        obj.save()
+
+    #form=request.POST('formulario')
+    #print(form)
+    # if request.method=='POST':
+    #     print("hola2")
+    #     username=request.POST['usuario']
+    #     first_name=request.POST['nombre']
+    #     last_name=request.POST['apellido']
+    #     email=request.POST['correo']
+    #     password=request.POST['contrasenha']
+    #     is_active=True
+    #     is_staff=False
+    #     is_superuser=False
+    #     reingresar_contrasenha=request.POST['reingresar_contrasenha']
+    #     if check_password(reingresar_contrasenha, password):
+    #         print("Coinciden")
+    #     else: 
+    #         messages.error (request, "La contraseña no coincide")
+    #     obj=User()
+    #     obj.username=username
+    #     obj.first_name=first_name
+    #     obj.last_name=last_name
+    #     obj.email=email
+    #     obj.password=password
+    #     obj.is_active=is_active
+    #     obj.is_staff=is_staff
+    #     obj.is_superuser=is_superuser
+    #     obj.date_joined=timezone.now()
+    #     obj.save()
+    #     print(obj)
     return render(request,"crear_usuarios.html",{})
+
--- scrum/scrum/urls.py
+++ scrum/scrum/urls.py
@@ -25,8 +25,7 @@ urlpatterns = [
     path('login',views.crear_usuario,name='crear_usuario'),
     path('inicio/',views.inicio, name='inicio'),
     path('cerrar/', views.cerrar, name='cerrar'),
-
-    
+    path('crear_proyecto',views.crear_proyecto,name='crear_proyecto'), 
 ]
 
 
